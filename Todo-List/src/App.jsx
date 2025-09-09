9b0f83cd1cf0d9075397f515548df46f05d67738
Sheeraz Ahmad <59965006+Sheeraz786955@users.noreply.github.com>
2025-09-09 16:26:55 +0500

    todo

---
+++ Todo-List/src/App.jsx
@@ -0,0 +1,47 @@
+import { useActionState, useState } from "react";
+import "./App.css";
+import Todo from "./components/Todo";
+
+function App() {
+  const [todo, setTodo] = useState([]);
+
+  const handleForm = (prevData, formData) => {
+    const name = formData.get("ADD");
+
+    if (!todo.includes(name)) {
+      let todolist = [...todo, name];
+      setTodo(todolist);
+    } else {
+      alert("TODO Already Exits");
+    }
+  };
+  const [data, action, pending] = useActionState(handleForm);
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-8 col-md-12 col-12 m-auto">
+            <div className="bg-color rounded-3 my-5 mx-sm-5 border border-2 border-primary">
+              <h1 className="text-white text-center py-4">TO DO LIST</h1>
+              <div className="text-center">
+                <form action={action}>
+                  <input
+                    className="m-1 me-2 py-1 ps-1"
+                    type="text"
+                    placeholder="Add ToDo"
+                    name="ADD"
+                  />
+                  <button className="btn btn-primary mb-1">ADD</button>
+                </form>
+              </div>
+              <h2 className="text-center text-white my-4">Task List</h2>
+              <Todo todo={todo} setTodoList={setTodo} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Todo-List/src/components/Todo.jsx
@@ -0,0 +1,38 @@
+
+
+function Todo({ todo, setTodoList }) {
+ 
+  const finalList = (index) => {
+    const newTodo = todo.filter((v, i) => i != index);
+    setTodoList(newTodo);
+  };
+  return (
+    <>
+      <div className="">
+        {todo.map((items, index) => (
+          <ul
+            className="list-unstyled border border-1 border-white rounded-3 m-4 position-relative"
+            key={index}
+          >
+            <li
+              className={`${
+                status ? "todocomplete" : ""
+              } text-white p-1 px-2 fw-bold`}
+             
+            >
+              <span className="pe-2">{index}</span>
+              {items}
+              <span className="position-absolute right-0">
+                <i
+                  className="fa-solid fa-trash text-danger pointer"
+                  onClick={() => finalList(index)}
+                ></i>
+              </span>
+            </li>
+          </ul>
+        ))}
+      </div>
+    </>
+  );
+}
+export default Todo;
