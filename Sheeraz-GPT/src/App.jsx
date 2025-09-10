7e8b37874dc94b7c9729fbd389906f4d06af29fb
Sheeraz Ahmad <59965006+Sheeraz786955@users.noreply.github.com>
2025-09-10 17:56:19 +0500

    impliment design

--- Sheeraz-GPT/src/App.jsx
+++ Sheeraz-GPT/src/App.jsx
@@ -7,7 +7,8 @@ import Answers from "./components/answers";
 
 function App() {
   const [question, setQuestion] = useState("");
-  const [answer, setAnswer] = useState("");
+  const [answer, setAnswer] = useState([]);
+  const [recentHistory, setRecentHistory] = useState([]);
   const askQuestion = async () => {
     const payLand = {
       contents: [
@@ -20,6 +21,16 @@ function App() {
         },
       ],
     };
+    if (localStorage.getItem("history")) {
+      let history = JSON.parse(localStorage.getItem("history"));
+      history = [question, ...history];
+      localStorage.setItem("history", JSON.stringify(history));
+      setRecentHistory(history);
+    } else {
+      localStorage.setItem("history", JSON.stringify([question]));
+      setRecentHistory(question);
+    }
+
     let response = await fetch(url, {
       method: "POST",
       body: JSON.stringify(payLand),
@@ -28,29 +39,74 @@ function App() {
     let dataString = response.candidates[0].content.parts[0].text;
     dataString = dataString.split("* ");
     dataString = dataString.map((item) => item.trim());
-    setAnswer(dataString);
+    setAnswer([
+      ...answer,
+      { type: "q", text: question },
+      { type: "a", text: dataString },
+    ]);
   };
-
+  console.log(recentHistory);
   return (
     <>
-      <div className="container-fluid">
+      <div className="container-fluid bg-black">
         <div className="row">
-          <div className="col-2 bg-primary-color">
-            <h1>Sheeraz GPT</h1>
-          </div>
-
-          <div className="col-10 screen_hight position-relative">
-            <div className="container overflow-auto h-75">
-              <ul className="list-unstyled">
-                {answer &&
-                  answer.map((item, index) => (
-                    <li className="text-white">
-                      <Answers ans={item} index={index} totelData={item.length} />
+          <div className="col-2 bg-primary-color text-white   ">
+            <h5 className="text-center my-3">
+              Recent History{" "}
+              <span>
+                <i className="fa-regular fa-trash-can"></i>
+              </span>
+            </h5>
+            <div className="sidebar-screen-hight overflow-auto">
+              <ul>
+                {recentHistory &&
+                  recentHistory.map((item, index) => (
+                    <li key={index} className="primary-color fs-5 fw-semibold">
+                      {item}
                     </li>
                   ))}
               </ul>
             </div>
-            <div className="w-75 m-auto bg-primary-color p-1 pt-3 rounded-4 px-3 input-position position-absolute">
+          </div>
+
+          <div className="col-10 screen_hight position-relative pt-3">
+            <div className="container overflow-auto hight-fit">
+              <div className="w-75 m-auto">
+                <ul className="list-unstyled">
+                  {answer.map((item, index) => (
+                    <div
+                      index={index + Math.random()}
+                      className={
+                        item.type == "q" ? "d-flex justify-content-end" : ""
+                      }
+                    >
+                      {item.type == "q" ? (
+                        <li className="text-white text-end fw-semibold border border-1 p-3 my-2  rounded-start-pill rounded-bottom-pill border-black bg-primary-color w-fit ">
+                          <Answers
+                            ans={item.text}
+                            index={index + Math.random()}
+                            totelData={1}
+                            type={item.type}
+                          />
+                        </li>
+                      ) : (
+                        item.text.map((ansItem, ansIndex) => (
+                          <li className="text-white fw-semibold my-2">
+                            <Answers
+                              ans={ansItem}
+                              index={ansIndex + Math.random()}
+                              totelData={item.length}
+                              type={item.type}
+                            />
+                          </li>
+                        ))
+                      )}
+                    </div>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="w-75 m-auto bg-primary-color p-1 pt-3 rounded-pill px-3 input-position position-absolute">
               <div className="input-group mb-3">
                 <input
                   type="text"
--- Sheeraz-GPT/src/components/answers.jsx
+++ Sheeraz-GPT/src/components/answers.jsx
@@ -1,21 +1,29 @@
 import { useEffect, useState } from "react";
 import { checkHeadings, replaceStarHeadings } from "../helper";
 
-function Answers({ ans, index, totelData }) {
-  const[heading,setHeading]=useState(false)
-  const[answer,setAnswer]=useState(ans)
+function Answers({ ans, index, totelData ,type }) {
+  const [heading, setHeading] = useState(false);
+  const [answer, setAnswer] = useState(ans);
   useEffect(() => {
-   if(checkHeadings(ans)){
-    setHeading(true)
-    setAnswer(replaceStarHeadings(ans)
-    )
-   }
-console.log(totelData)
-
+    if (checkHeadings(ans)) {
+      setHeading(true);
+      setAnswer(replaceStarHeadings(ans));
+    }
+    console.log(totelData);
   }, []);
 
-  return <>{index&&totelData>1?<h6 className="py-2" key={index}>{answer}</h6>:
-  heading? <h5 className="pt-3">{answer}</h5>:<span className="primary-color ps-4">{answer}</span>}
-</>
+  return (
+    <>
+      {index && totelData > 1 ? (
+        <h6 className="py-2 fw-bolder" key={index}>
+          {answer}
+        </h6>
+      ) : heading ? (
+        <h5 className="pt-3">{answer}</h5>
+      ) : (
+        <span className={type=="q"?"ps-1 primary-color":"primary-color ps-4"}>{answer}</span>
+      )}
+    </>
+  );
 }
 export default Answers;
